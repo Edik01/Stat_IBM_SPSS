4d55575dff41a72c61c952a02ff016b73a1edd6a
Edik Huloian <guloyan.edik@gmail.com>
2025-07-21 19:07:35 -0700

    update

--- protect.py
+++ protect.py
@@ -1,3 +1,14 @@
 import psutil
 
-suspicious_processes = ["keylogger", "pynput", "stealer", "logger", "rat", ]
+suspicious_keywords = ["keylogger", "pynput", "stealer", "logger", "rat"]
+
+for proc in psutil.process_iter(['pid', 'name', 'cmdline']):
+    try:
+        info = proc.info
+        command = " ".join(info['cmdline']) if info['cmdline'] else ''
+        for keyword in suspicious_keywords:
+            if keyword.lower() in command.lower():
+                print(f"[!] Suspicious process detected: {info['name']} (PID: {info['pid']})")
+                print(f"    Command: {command}")
+    except (psutil.NoSuchProcess, psutil.AccessDenied):
+        continue
